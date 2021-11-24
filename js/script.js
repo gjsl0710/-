8460c0ce1b47c165c03cc7091130e8589d116a00
gjsl0710 <67987471+gjsl0710@users.noreply.github.com>
2021-11-24 14:53:02 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-var images = ['https://imgur.com/D93WCfM.png'];
+var images = ['https://cdn.discordapp.com/attachments/911919436057935882/912941034286235690/KakaoTalk_20210724_164006437_01.gif'];
 
 $('#container').append('<style>#container, .acceptContainer:before, .logoContainer:before {background: url(' + images[Math.floor(Math.random() * images.length)] + ') center fixed }');
