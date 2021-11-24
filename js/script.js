614d4c9a65f25d31e6a9ba322d945c4ab2a2d691
gjsl0710 <67987471+gjsl0710@users.noreply.github.com>
2021-11-24 14:55:09 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-var images = ['https://cdn.discordapp.com/attachments/911919436057935882/912941034286235690/KakaoTalk_20210724_164006437_01.gif'];
+var images = ['https://cdn.discordapp.com/attachments/911919436057935882/912944201560379452/88775396_p0.png', 'https://cdn.discordapp.com/attachments/911919436057935882/912944201874931742/shark_2_1.png'];
 
 $('#container').append('<style>#container, .acceptContainer:before, .logoContainer:before {background: url(' + images[Math.floor(Math.random() * images.length)] + ') center fixed }');
