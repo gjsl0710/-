49332111537260cdef1a3f63ce3339040bf2a84f
gjsl0710 <67987471+gjsl0710@users.noreply.github.com>
2021-12-19 17:48:05 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-var images = ['https://cdn.discordapp.com/attachments/911919436057935882/912944201560379452/88775396_p0.png', 'https://cdn.discordapp.com/attachments/911919436057935882/912944201874931742/shark_2_1.png'];
+var images = ['https://cdn.discordapp.com/attachments/911919436057935882/922046718734905354/unknown.png'];
 
 $('#container').append('<style>#container, .acceptContainer:before, .logoContainer:before {background: url(' + images[Math.floor(Math.random() * images.length)] + ') center fixed }');
